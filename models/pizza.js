5eee9b2e0d8617eb23df718b8e6af351d5823f05
Xcamaj <Xhemajlcamaj@gmail.com>
2022-04-27 13:06:52 -0400

    good

--- models/pizza.js
+++ models/pizza.js
@@ -1,6 +1,4 @@
 const { Schema, model } = require('mongoose');
-const Pizza = model('Pizza', PizzaSchema);
-
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -19,5 +17,7 @@ const PizzaSchema = new Schema({
     },
     toppings: []
 });
+
+const Pizza = model('Pizza', PizzaSchema);
 
 module.exports = Pizza;
